4a123e26cb14c28843a4d7e2e0e10d0409157881
swendleys <sprott95@gmail.com>
2017-10-12 16:03:16 +0200

    'testing'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NguiMapModule} from '@ngui/map';
 
 import { AppComponent } from './app.component';
+import {GoogleMapsComponent} from "./map/map.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyD0l8AD86NucXhybctdjkzL32XE7PDsYIA'})
   ],
   providers: [],
   bootstrap: [AppComponent]
